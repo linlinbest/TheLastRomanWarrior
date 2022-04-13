5a39914b5f0eab978dfbc6e00543157326a0449d
linlinbest <444053358@qq.com>
2022-04-13 16:51:22 -0400

    Move the project to a new version

---
+++ Assets/testScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void holdEvent()
+    {
+        Debug.Log("Output event");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
