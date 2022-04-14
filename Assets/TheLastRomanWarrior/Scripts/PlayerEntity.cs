7f1dd09e7674c7c986ca3213f69371c1abdd5bbc
linlinbest <444053358@qq.com>
2022-04-14 14:26:09 -0400

    Add a PlayerEntity class

---
+++ Assets/TheLastRomanWarrior/Scripts/PlayerEntity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : MonoBehaviour
+{
+    [SerializeField]
+    private health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
