98be1eeb0aabe7d887d741a9a4d7892bd6676f44
HanLinSun <49753678+HanLinSun@users.noreply.github.com>
2022-04-14 16:44:01 -0400

    animation update

---
+++ Assets/TheLastRomanWarrior/Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator enemyAnim;
+    [SerializeField] private GameObject player;
+    [SerializeField] private bool isAttack;
+    [SerializeField] private float moveSpeed;
+    //self
+    private GameObject enemyObj;
+    
+    
+    void Awake()
+    {
+        enemyAnim = this.GetComponent<Animator>();
+        player=GameObject.FindGameObjectWithTag("Player");
+        isAttack = false;
+        moveSpeed = 20f;
+        enemyObj = this.gameObject;
+    }
+    //
+    void enemyMove()
+    {
+     
+        
+    }
+
+    void enemyAttack()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
