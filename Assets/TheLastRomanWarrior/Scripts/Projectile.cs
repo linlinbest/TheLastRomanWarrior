106d5eb8def00f8be92cd226b8c30d9c3ef66d75
linlinbest <444053358@qq.com>
2022-04-14 16:44:29 -0400

    Add Javelin and Enemy class

--- Assets/TheLastRomanWarrior/Scripts/Projectile.cs
+++ Assets/TheLastRomanWarrior/Scripts/Projectile.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerEntity : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
-    [SerializeField]
-    private int health = 5;
-
     // Start is called before the first frame update
     void Start()
     {
